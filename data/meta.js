2c1f3e020ac92bb42520c2983684a9fa8825e97b
Briant_real_estate <hooni8602@gmail.com>
2026-08-20 08:20:07 +0900

    data: 2026-08-20 08:20

--- data/meta.js
+++ data/meta.js
@@ -1,1 +1,1 @@
-window.HUB_DATA=window.HUB_DATA||{};window.HUB_DATA.meta={"generated_at": "2026-08-19 08:39", "listings_count": 8387, "results_count": 7983};
+window.HUB_DATA=window.HUB_DATA||{};window.HUB_DATA.meta={"generated_at": "2026-08-20 08:20", "listings_count": 7815, "results_count": 8419};
